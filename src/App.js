764beaf331dd241f06f06fa65cf75104bf534cb9
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-02-16 16:21:47 -0300

    initail setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home Ever been in a room and felt like something
+            was missing? Perhaps it felt slightly bare and uninviting. I’ve got
+            some simple tips to help you make any room feel complete. Michelle
+            Appleton 28 Jun 2020 Share
+            <div className="attribution">
+                Challenge by{" "}
+                <a
+                    href="https://www.frontendmentor.io?ref=challenge"
+                    target="_blank"
+                >
+                    Frontend Mentor
+                </a>
+                . Coded by <a href="#">Your Name Here</a>.
+            </div>
+        </>
+    );
 }
-
-export default App;
